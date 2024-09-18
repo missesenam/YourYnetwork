0a141e83efbb16e6b160e1f02c84c1f98ab00e93
missesenam <moneyesenam@gmail.com>
2024-09-18 22:52:01 +0100

    homepage responsiveness

--- main.js
+++ main.js
@@ -14,3 +14,15 @@ topBtn.addEventListener('click', ()=>{
         behavior: 'smooth'
     });
 })
+// hide and show menu
+
+const menuBar = document.getElementById('menu-bar')
+const hamburgerbar = document.getElementById('showlinks')
+function showsidebar(){
+    menuBar.classList.remove('hidden')
+    hamburgerbar.classList.add('hidden')
+}
+function hidesidebar(){ 
+    menuBar.classList.add('hidden')
+    hamburgerbar.classList.remove('hidden')
+} 
